3354b19b472bdf52d84f41ff40e51fe41418cd79
Pedro Guedes <pedicoq@gmail.com>
2020-12-31 15:58:13 +0000

    feat: add cookie plugin

--- src/app.js
+++ src/app.js
@@ -2,22 +2,24 @@
 
 const path = require('path');
 const AutoLoad = require('fastify-autoload');
-
+const cookie = require('fastify-cookie');
 const database = require('./database');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
   await database.sync();
 
+  fastify.register(cookie);
+
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts),
-  });
+  // fastify.register(AutoLoad, {
+  //   dir: path.join(__dirname, 'plugins'),
+  //   options: Object.assign({}, opts),
+  // });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
