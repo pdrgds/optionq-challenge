7501d2ea6396eef4266c2fda1091605b775c505d
Pedro Guedes <pedicoq@gmail.com>
2020-12-31 13:56:31 +0000

    fix: session/create return value

--- src/services/session.js
+++ src/services/session.js
@@ -4,15 +4,15 @@ const { v4: uuidv4 } = require("uuid");
 function create(userHandle) {
   const id = uuidv4();
 
-  await models.sessions.create({ id, userHandle})
+  return models.sessions.create({ id, userHandle });
 }
 
-function check(id){
-    return models.sessions.findOne({where: { id }})
+function check(id) {
+  return models.sessions.findOne({ where: { id } });
 }
 
-function destroy(id){
-    return models.sessions.destroy({where: {id}})
+function destroy(id) {
+  return models.sessions.destroy({ where: { id } });
 }
 
-module.exports = {create, check, destroy}
+module.exports = { create, check, destroy };
