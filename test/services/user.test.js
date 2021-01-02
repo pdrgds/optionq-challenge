075ff27752f7e30f06ba269a8c0c7a9404d519d7
Pedro Guedes <pedicoq@gmail.com>
2021-01-02 20:00:15 +0000

    fix: uncomment tests

--- test/services/user.test.js
+++ test/services/user.test.js
@@ -7,35 +7,35 @@ const { build } = require('../helper');
 const services = require('../../src/services');
 
 testWithDb('user service', () => {
-  // test('user can follow another user', async (t) => {
-  //   const test1 = await services.user.create('test1', 'email@example.com', '12345');
-  //   const test2 = await services.user.create('test2', 'email2@example.com', '123456');
+  test('user can follow another user', async (t) => {
+    const test1 = await services.user.create('test1', 'email@example.com', '12345');
+    const test2 = await services.user.create('test2', 'email2@example.com', '123456');
 
-  //   await services.user.follow('test1', 'test2');
+    await services.user.follow('test1', 'test2');
 
-  //   await test1.reload();
-  //   await test2.reload();
+    await test1.reload();
+    await test2.reload();
 
-  //   t.ok(test1.following.includes('test2'));
-  //   t.ok(test2.followers.includes('test1'));
+    t.ok(test1.following.includes('test2'));
+    t.ok(test2.followers.includes('test1'));
 
-  //   t.end();
-  // });
+    t.end();
+  });
 
-  // test('following count should return correct value', async (t) => {
-  //   await services.user.create('test1', 'email@example.com', '1234');
-  //   await services.user.create('test2', 'email2@example.com', '5678');
-  //   await services.user.create('test3', 'email3@example.com', '98712');
+  test('following count should return correct value', async (t) => {
+    await services.user.create('test1', 'email@example.com', '1234');
+    await services.user.create('test2', 'email2@example.com', '5678');
+    await services.user.create('test3', 'email3@example.com', '98712');
 
-  //   await services.user.follow('test1', 'test2');
-  //   await services.user.follow('test1', 'test3');
+    await services.user.follow('test1', 'test2');
+    await services.user.follow('test1', 'test3');
 
-  //   const followingCount = await services.user.getFollowingCount('test1');
+    const followingCount = await services.user.getFollowingCount('test1');
 
-  //   t.same(followingCount, 2);
+    t.same(followingCount, 2);
 
-  //   t.end();
-  // });
+    t.end();
+  });
 
   test('should login and logout correctly with an existing user', async (t) => {
     const app = build(t);
